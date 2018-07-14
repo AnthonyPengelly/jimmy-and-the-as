35db98cb43c588704863d4f35cae649a075541dc
Anthony Pengelly <Anthony.Pengelly@Softwire.com>
2018-07-14 16:40:49 +0100

    Example using Boostrap react

--- src/components/hello.tsx
+++ src/components/hello.tsx
@@ -1,9 +1,16 @@
 import * as React from "react";
+import { Button } from "react-bootstrap";
 
 export interface HelloProps { compiler: string; framework: string; }
 
 export class Hello extends React.Component<HelloProps, {}> {
     render() {
-        return <h1>Hello from {this.props.compiler} and {this.props.framework}!</h1>;
+        return (
+            <div>
+                <h1>Hello from {this.props.compiler} and {this.props.framework}!</h1>
+                <Button bsStyle="primary">Primary Button</Button>
+            </div>
+            
+        );
     }
 }

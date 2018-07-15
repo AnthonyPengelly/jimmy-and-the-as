e4ca1d4dd74034dc331e3fc1b97221ee59b194d0
Anthony Pengelly <Anthony.Pengelly@Softwire.com>
2018-07-15 23:26:48 +0100

    Add a carousel with images

--- src/components/contact/contact.tsx
+++ src/components/contact/contact.tsx
@@ -1,4 +1,6 @@
 import * as React from "react";
+import { Row, Col } from "react-bootstrap";
+
 import Facebook from "react-icons/fa/facebook-square";
 import Twitter from "react-icons/fa/twitter-square";
 import Mail from "react-icons/fa/envelope";
@@ -9,23 +11,28 @@ const ICON_SIZE = 30;
 export class Contact extends React.Component<{}, {}> {
     render() {
         return (
-            <div>
-                <h1>Contact</h1>
-                <p>Get in touch to find out more or book us for your event!</p>
-                <p><a href="mailto:jimmyandtheas@gmail.com">
-                    <Mail size={ICON_SIZE} color="#D34836" /> JimmyAndTheAs@gmail.com
-                </a></p>
-                <p><a href="https://facebook.com/jimmyandtheas" target="_blank">
-                    <Facebook size={ICON_SIZE} color="#3B5998" /> https://facebook.com/jimmyandtheas
-                </a></p>
-                <p><a href="https://twitter.com/jimmyandtheas" target="_blank">
-                    <Twitter size={ICON_SIZE} color="#1DA1F2" /> @JimmyAndTheAs
-                </a></p>
-                <p><a href="https://instagram.com/jimmyandtheas" target="_blank">
-                    <Instagram size={ICON_SIZE} color="#000000" /> @jimmyandtheas
-                </a></p>
-                <p>We look forward to hearing from you</p>
-            </div>
+            <Row>
+                <Col xs={12} md={4}>
+                    <h1>Contact</h1>
+                    <p>Get in touch to find out more or book us for your event!</p>
+                    <p><a href="mailto:jimmyandtheas@gmail.com">
+                        <Mail size={ICON_SIZE} color="#D34836" /> JimmyAndTheAs@gmail.com
+                    </a></p>
+                    <p><a href="https://facebook.com/jimmyandtheas" target="_blank">
+                        <Facebook size={ICON_SIZE} color="#3B5998" /> https://facebook.com/jimmyandtheas
+                    </a></p>
+                    <p><a href="https://twitter.com/jimmyandtheas" target="_blank">
+                        <Twitter size={ICON_SIZE} color="#1DA1F2" /> @JimmyAndTheAs
+                    </a></p>
+                    <p><a href="https://instagram.com/jimmyandtheas" target="_blank">
+                        <Instagram size={ICON_SIZE} color="#000000" /> @jimmyandtheas
+                    </a></p>
+                    <p>We look forward to hearing from you</p>
+                </Col>
+                <Col xs={12} md={8}>
+                    <img className="full-size-image" src="/images/band.jpg" alt="Jimmy &amp; the As" />
+                </Col>
+            </Row>
         );
     }
 }
--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -1,12 +1,27 @@
 import * as React from "react";
-import { Link } from "react-router-dom"
+import { Carousel } from "react-bootstrap"
+
+const CAROUSEL_IMAGES = [
+    {name: "Trumpet", path: "/images/trumpet.jpg"},
+    {name: "Mention Me 1", path: "/images/mention-me1.jpg"},
+    {name: "Band", path: "/images/band-cropped.jpg"},
+    {name: "Mention Me 2", path: "/images/mention-me2.jpg"},
+];
 
 export class Home extends React.Component<{}, {}> {
     render() {
+        const carouselItems = CAROUSEL_IMAGES.map((image, index) => (
+            <Carousel.Item key={index}>
+                <img className="carousel-image" src={image.path} alt={image.name} />
+            </Carousel.Item>
+        ));
+
         return (
             <div>
                 <h1>Jimmy and the As</h1>
-                <h2>Coming soon!</h2>
+                <Carousel>
+                    {carouselItems}
+                </Carousel>
             </div>
         );
     }

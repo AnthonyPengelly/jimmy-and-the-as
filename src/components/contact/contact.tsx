ca279a4d1187b834d51ce13a19cfc392ed3dd04e
Anthony Pengelly <Anthony.Pengelly@Softwire.com>
2018-07-16 19:39:14 +0100

    Move contact details into config

--- src/components/contact/contact.tsx
+++ src/components/contact/contact.tsx
@@ -1,9 +1,11 @@
 import * as React from "react";
 import { Row, Col } from "react-bootstrap";
+import Config from "../../config";
 
+import Mail from "react-icons/fa/envelope";
+import Phone from "react-icons/fa/phone";
 import Facebook from "react-icons/fa/facebook-square";
 import Twitter from "react-icons/fa/twitter-square";
-import Mail from "react-icons/fa/envelope";
 import Instagram from "react-icons/fa/instagram";
 
 const ICON_SIZE = 30;
@@ -15,17 +17,20 @@ export class Contact extends React.Component<{}, {}> {
                 <Col xs={12} md={4}>
                     <h1>Contact</h1>
                     <p>Get in touch to find out more or book us for your event!</p>
-                    <p><a href="mailto:jimmyandtheas@gmail.com">
-                        <Mail size={ICON_SIZE} color="#D34836" /> JimmyAndTheAs@gmail.com
+                    <p><a href={"mailto:" + Config.email}>
+                        <Mail size={ICON_SIZE} color="#D34836" /> {Config.email}
                     </a></p>
-                    <p><a href="https://facebook.com/jimmyandtheas" target="_blank">
-                        <Facebook size={ICON_SIZE} color="#3B5998" /> https://facebook.com/jimmyandtheas
+                    <p><a href={"tel:" + Config.phone}>
+                        <Phone size={ICON_SIZE} /> {Config.phone}
                     </a></p>
-                    <p><a href="https://twitter.com/jimmyandtheas" target="_blank">
-                        <Twitter size={ICON_SIZE} color="#1DA1F2" /> @JimmyAndTheAs
+                    <p><a href={Config.facebook} target="_blank">
+                        <Facebook size={ICON_SIZE} color="#3B5998" /> {Config.facebook}
                     </a></p>
-                    <p><a href="https://instagram.com/jimmyandtheas" target="_blank">
-                        <Instagram size={ICON_SIZE} color="#000000" /> @jimmyandtheas
+                    <p><a href={Config.twitter} target="_blank">
+                        <Twitter size={ICON_SIZE} color="#1DA1F2" /> {Config.twitterHandle}
+                    </a></p>
+                    <p><a href={Config.instagram} target="_blank">
+                        <Instagram size={ICON_SIZE} color="#000000" /> {Config.instagramHandle}
                     </a></p>
                     <p>We look forward to hearing from you</p>
                 </Col>
---
+++ src/config/index.ts
@@ -0,0 +1,9 @@
+export default {
+    email: "JimmyAndTheAs@gmail.com",
+    facebook: "https://facebook.com/jimmyandtheas",
+    twitter: "https://twitter.com/jimmyandtheas",
+    twitterHandle: "@JimmyAndTheAs",
+    instagram: "https://instagram.com/jimmyandtheas",
+    instagramHandle: "@jimmyandtheas",
+    phone: "+447963355148"
+};

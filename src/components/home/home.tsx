e4bc8f845f908b9aedcfcf3a23c124669bc5b978
Anthony Pengelly <Anthony.Pengelly@Softwire.com>
2018-07-16 23:02:22 +0100

    Minor styling improvements and added a favicon

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -4,8 +4,8 @@ export class Home extends React.Component<{}, {}> {
     render() {
         return (
             <div>
-                <h1>Jimmy and the As</h1>
-                <h2>Home - Coming soon!</h2>
+                <h1>Home</h1>
+                <h2>Coming soon!</h2>
                 <img className="full-size-image" src="/images/mention-me1.jpg" alt="Jimmy &amp; the As" />
             </div>
         );

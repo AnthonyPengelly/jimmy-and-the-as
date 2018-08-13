7289aa91f9e491a007b1011f5dc9e11528227597
Anthony Pengelly <Anthony.Pengelly@Softwire.com>
2018-08-13 19:46:35 +0100

    Update the songs to use the new recordings

--- src/assets/audio.ts
+++ src/assets/audio.ts
@@ -6,29 +6,24 @@ export interface AudioItem {
 
 const audio: AudioItem[] = [
     {
+        url: "/audio/Brown Eyed Girl.mp3",
+        title: "Brown Eyed Girl",
+        artist: "Van Morrison"
+    },
+    {
         url: "/audio/Umbrella.mp3",
         title: "Umbrella",
         artist: "Rihanna"
     },
     {
-        url: "/audio/10 out of 10.mp3",
-        title: "10/10 (live)",
-        artist: "Paolo Nutini"
-    },
-    {
-        url: "/audio/Pencil full of lead.mp3",
-        title: "Pencil Full of Lead (live)",
-        artist: "Paolo Nutini"
+        url: "/audio/Problem.mp3",
+        title: "Problem",
+        artist: "Ariana Grande"
     },
     {
         url: "/audio/toxic.mp3",
-        title: "Toxic (live)",
+        title: "Toxic",
         artist: "Britney Spears"
-    },
-    {
-        url: "/audio/No diggity.mp3",
-        title: "No Diggity (live)",
-        artist: "Blackstreet"
     },
 ];
 export default audio;
